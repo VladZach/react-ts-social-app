4530ab89b8b32654d76038c95f1aa87ab829e718
VladZach <vladzach25@gmail.com>
2021-02-09 22:40:20 +0300

    Added form validation with formi to signup

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,45 +1,94 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 export default function SignUp() {
-  return (
-    <div className="page page_centralized">
-      <div className="card">
-        <h2 className="card__item card__header">Sign Up</h2>
-        <form className="card__form card__item form">
+  interface formValues {
+    email?: string;
+    password?: string;
+    passwordConfirmation?: string;
+  }
+
+  function validate(values: formValues) {
+    const errors: formValues = {};
+    if (!values.email) {
+      errors.email = "Required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = "Invalid email address";
+    }
+    if (!values.password) {
+      errors.password = "Required";
+    } else if (values.password && values.password.length < 6) {
+      errors.password = "Password is shorter then 6 letters";
+    }
+    if (!values.passwordConfirmation) {
+      errors.passwordConfirmation = "Required";
+    } else if (values.passwordConfirmation !== values.password) {
+      errors.passwordConfirmation = "Passwords doesn't match";
+    }
+    return errors;
+  }
+
+  const form = (
+    <Formik
+      initialValues={{ email: "", password: "", passwordConfirmation: "" }}
+      validate={validate}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form className="card__form card__item form">
           <div className="form__item">
             <label className="form__label" htmlFor="email">
               Email
             </label>
-            <input
+            <Field
               className="form__input form__input_textual-sm"
-              name="email"
               type="email"
+              name="email"
             />
+            <ErrorMessage name="email" component="div" />
           </div>
           <div className="form__item">
             <label className="form__label" htmlFor="password">
               Password
             </label>
-            <input
+            <Field
               className="form__input form__input_textual-sm"
-              name="password"
               type="password"
+              name="password"
             />
+            <ErrorMessage name="password" component="div" />
           </div>
           <div className="form__item">
-            <label className="form__label" htmlFor="password-confirmation">
-              Email
+            <label className="form__label" htmlFor="password">
+              Password Confirmation
             </label>
-            <input
+            <Field
               className="form__input form__input_textual-sm"
-              name="password-confirmation"
               type="password"
+              name="passwordConfirmation"
             />
+            {/* имя должно точно соответствовать имени свойства в объекте, то есть кэмэлКейс */}
+            <ErrorMessage name="passwordConfirmation" component="div" />
           </div>
+
           <button className="button form__submit-button" type="submit">
             Sign Up
           </button>
-        </form>
+        </Form>
+      )}
+    </Formik>
+  );
+
+  return (
+    <div className="page page_centralized">
+      <div className="card">
+        <h2 className="card__item card__header">Sign Up</h2>
+        {form}
         <div className="card__footer card__item">
           <a>Already have an account?</a>
         </div>
